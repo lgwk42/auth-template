47dbabf5532ae614e052b3520b0ae0721eefc50b
lgwk42 <luisgunkim@gmail.com>
2024-11-12 14:37:20 +0900

    :fire: :: unused import delete

--- src/main/java/com/project/authtemplate/domain/auth/service/response/RefreshTokenResponse.java
+++ src/main/java/com/project/authtemplate/domain/auth/service/response/RefreshTokenResponse.java
@@ -2,8 +2,6 @@ package com.project.authtemplate.domain.auth.service.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
