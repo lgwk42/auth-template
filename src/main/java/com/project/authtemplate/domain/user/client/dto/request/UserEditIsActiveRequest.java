eb0dfdf378763ea45e49903e0e79f51b645e9be4
lgwk42 <luisgunkim@gmail.com>
2025-09-17 09:42:20 +0900

    :fire: delete unused class

--- src/main/java/com/project/authtemplate/domain/user/client/dto/request/UserEditIsActiveRequest.java
+++
@@ -1,7 +0,0 @@
-package com.project.authtemplate.domain.user.client.dto.request;
-
-public record UserEditIsActiveRequest(
-        String email,
-        boolean isActive
-) {
-}
--- src/main/java/com/project/authtemplate/domain/user/client/dto/request/UserEditRequest.java
+++
@@ -1,8 +0,0 @@
-package com.project.authtemplate.domain.user.client.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserEditRequest(
-        @NotBlank(message = "이름을 필수입니다.")
-        String name
-) {}
