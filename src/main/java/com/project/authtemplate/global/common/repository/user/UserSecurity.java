9d55159ea83386c9fcf2452c470df573b058a37c
lgwk42 <luisgunkim@gmail.com>
2026-02-23 10:59:00 +0900

    :sparkles: add getUserOrNull method to UserSecurity

--- src/main/java/com/project/authtemplate/global/common/repository/user/UserSecurity.java
+++ src/main/java/com/project/authtemplate/global/common/repository/user/UserSecurity.java
@@ -1,9 +1,11 @@
 package com.project.authtemplate.global.common.repository.user;
 
-import com.project.authtemplate.domain.user.client.dto.User;
+import com.project.authtemplate.domain.user.domain.model.User;
 
 public interface UserSecurity {
 
     User getUser();
+
+    User getUserOrNull();
 
 }
--- src/main/java/com/project/authtemplate/global/common/repository/user/impl/UserSecurityImpl.java
+++ src/main/java/com/project/authtemplate/global/common/repository/user/impl/UserSecurityImpl.java
@@ -1,12 +1,13 @@
 package com.project.authtemplate.global.common.repository.user.impl;
 
-import com.project.authtemplate.domain.user.client.dto.User;
+import com.project.authtemplate.domain.user.domain.model.User;
 import com.project.authtemplate.global.common.repository.user.UserSecurity;
 import com.project.authtemplate.global.security.auth.CustomUserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
-@Repository
+@Component
 public class UserSecurityImpl implements UserSecurity {
 
     @Override
@@ -16,6 +17,15 @@ public class UserSecurityImpl implements UserSecurity {
                 .getAuthentication()
                 .getPrincipal())
                 .getUser();
+    }
+
+    @Override
+    public User getUserOrNull() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails details)) {
+            return null;
+        }
+        return details.getUser();
     }
 
 }
