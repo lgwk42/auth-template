0406b2ccfb1c9176be5884aee40644e0d2ba0544
lgwk42 <luisgunkim@gmail.com>
2026-02-23 10:59:11 +0900

    :wrench: convert JwtProperties to record with issuer support

---
+++ src/main/java/com/project/authtemplate/global/security/jwt/config/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.project.authtemplate.global.security.jwt.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class JwtConfig {
+
+}
--- src/main/java/com/project/authtemplate/global/security/jwt/config/JwtProperties.java
+++ src/main/java/com/project/authtemplate/global/security/jwt/config/JwtProperties.java
@@ -1,18 +1,13 @@
 package com.project.authtemplate.global.security.jwt.config;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Getter
-@Setter
-@Configuration
 @ConfigurationProperties(prefix = "application.jwt")
-public class JwtProperties {
-
-    private String secretKey;
-    private long expiration;
-    private long refreshExpiration;
+public record JwtProperties(
+        String secretKey,
+        long expiration,
+        long refreshExpiration,
+        String issuer
+) {
 
 }
