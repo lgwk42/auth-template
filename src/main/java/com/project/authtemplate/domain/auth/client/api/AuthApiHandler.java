c49608ec562faf902cd9feaf2cc9f084f5de6321
lgwk42 <luisgunkim@gmail.com>
2026-02-23 10:58:48 +0900

    :art: add @Validated annotation to AuthApiHandler

--- src/main/java/com/project/authtemplate/domain/auth/client/api/AuthApiHandler.java
+++ src/main/java/com/project/authtemplate/domain/auth/client/api/AuthApiHandler.java
@@ -15,8 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
